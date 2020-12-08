10b0659c5de9a85d1fa9002591910eb5c70eb8a2
rain <2368001047@qq.com>
2020-12-08 11:20:39 +0800

    12.08 11:20

--- src/main/java/arrdemo.java
+++ src/main/java/arrdemo.java
@@ -5,7 +5,7 @@ public class arrdemo {
         int[] arr = new int[3];
 //        不指定，初始值为0
         for (int i = 0; i < 3; i++) {
-            System.out.println(arr[i]);
+            System.out.println("角标为" + i + "的数组值为:" + arr[i]);
             System.out.println("练习idea上传GitHub");
             System.out.println("练习idea上传GitHub，但GitHub上已经有新内容");
         }
