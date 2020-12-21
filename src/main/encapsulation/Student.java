3e6130c18a8b0fb78acf287eb3761e22119b6afe
rain <2368001047@qq.com>
2020-12-21 17:29:17 +0800

    17:29

---
+++ src/main/encapsulation/Student.java
@@ -0,0 +1,27 @@
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author RainZero
+ * @date 2020/12/21 14:48
+ * 代码的封装，创建私有的成员变量
+ */
+public class Student {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
